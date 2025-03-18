1cb86424d063177f1e6d751a43389fc9da8dc015
Debaa <debaa.dm@gmail.com>
2025-03-18 13:53:33 +0530

    Project Zip extracted

---
+++ include/menubar.php
@@ -0,0 +1,69 @@
+<?php include 'include/dbconn.php';
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <div class="container">
+        <!--a href="view.php" class="navbar-brand">CMS SYSTEM</a-->
+        <!--label class= "navbar-brand">CMS System</label-->
+        <span class="navbar-brand">CMS System</span>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="view.php">Home</a>
+                </li>
+                <?php
+                //error_reporting(0);// This hides all error warnings.
+                $sel_cat = "select distinct category from posts";
+                $run_cat = mysqli_query($conn, $sel_cat);
+                $state = "";
+
+                while ($rows = mysqli_fetch_assoc($run_cat)) {
+                    if (isset($_GET['cate']) && ($_GET['cate'] == $rows['category'])) {
+                        $state = "active";
+                    }
+                    else {
+                        $state = "";
+
+                    }
+                    echo '<li class="nav-item">
+                    <a class="nav-link ' . $state . '" aria-current="page" href="categoryview.php?cate=' 
+                    . $rows['category'] . '">' . ucfirst($rows['category']) . '</a> </li>';
+                }
+
+
+
+                /*
+               $category=$_GET['cate'];
+                
+                while($rows=mysqli_fetch_assoc( $run_cat))
+                {   
+                    if($category)
+                    {
+                    if($_GET['cate']==$rows['category'])
+                    {
+                        $state="active";
+                        
+                    }
+                    else
+                    { $state=""; }
+                }
+                
+                    echo '<li class="nav-item">
+                    <a class="nav-link '.$state.'" aria-current="page" href="categoryview.php?cate='.$rows['category'].'">'.ucfirst($rows['category']).'</a>
+                </li>';
+                
+                }*/
+
+
+                ?>
+
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
