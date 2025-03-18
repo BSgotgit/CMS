b2b51ddc044c14cdb6ca7c4f04a61eeb352340b9
Debaa <debaa.dm@gmail.com>
2025-03-18 22:16:18 +0530

    Searching Functionality Added

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
     <script src="bootstrap/js/bootstrap.js"></script>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <!-- TODO add an offline "search icon" -->
 </head>
 
 <body>
---
+++ search.php
@@ -0,0 +1,165 @@
+<html>
+
+<head>
+    <title>CMS</title>
+    <script src="jQry/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <?php include 'include/menubar.php'; ?>
+    <br>
+
+    <div class="container">
+        <div class="row">
+
+
+            <!--   LEFT SIDE MAIN POSTS  -->
+
+            <section class="col-lg-7">
+            <?php
+
+            // Getting the search query and removing extra spaces
+            $query = isset($_GET['query']) ? trim($_GET['query']) : '';
+
+            if (!empty($query)) {
+                //Removing Escape characters (For Safety)
+                $query = $conn->real_escape_string($query);
+                    
+                // Breaking the search query into keywords
+                $keywords = explode(" ", $query);
+                
+                // Build search condition
+                $searchConditions = [];
+                foreach ($keywords as $word) {
+                    $searchConditions[] = " `title` LIKE '%$word%' OR `description` LIKE '%$word%'";
+                }
+                
+                $sel_sql = "SELECT * FROM posts WHERE " . implode(" OR ", $searchConditions) . " ORDER BY 
+                            (CASE WHEN `title` LIKE '%$query%' THEN 3
+                                  WHEN `description` LIKE '%$query%' THEN 2
+                                  ELSE 1 END) DESC";
+                
+                $runs_sql = mysqli_query($conn, $sel_sql);
+
+                
+                if($runs_sql->num_rows > 0 ){
+                    // Matching post found
+                    while ($row = mysqli_fetch_assoc($runs_sql)) {
+                        echo '<div class="card">
+                                <div class="card-header">
+                                            <strong>' . $row['title'] . '</strong>
+                                        </div>
+                                    <div class="card-body">
+    
+                                        <div class="row">
+                                        <div class="col-lg-9">
+                                        <img src=" ' . $row['image'] . '" width="100%" alt="..." class="">
+                                        </div>
+                                        <div class="col-lg-12">
+                                        <p class="card-text">' . substr($row['description'], 0, 150) . '..... 
+                                            </p>
+                                        </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class=""></div>
+                                            <div class=""> <a href="readpost.php?pstd=' . $row['post_id'] . '" class="btn btn-secondary">Read More</a></div>
+                                        </div>
+    
+                                    </div>
+    
+                                </div>';
+                    }
+                }
+                else {
+                    echo 'No Matching Post Found';
+                }
+            }
+            else {
+                // No valid Query, So, Redirect to the home page
+                header("Location: index.php");
+                exit;
+            }
+            ?>
+            </section>
+
+
+            <!--   RIGHT SIDE CONTENTS  -->
+
+            <aside class="col-lg-5">
+
+                <!--     SEARCH BOX  -->
+
+                <form role="form" action="search.php" method="get">
+                    <div class="card">
+
+                        <div class="card-header">Search Something</div>
+
+                        <div class="card-body">
+                            <div class="row mb-2">
+                                <div class="input-group">
+                                    <input type="search" class="form-control" name="query" placeholder="Search something">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-default" type="submit">
+                                            <i class="bi bi-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+
+                <!--    LOGIN FORM  -->
+
+                <form role="form" action="">
+                    <div class="card">
+                        <div class=" card-header">
+                            Login Area
+                        </div>
+                        <div class="card-body">
+
+                            <div class="row mb-2">
+                                <label for="username" class="col-sm-4 col-form-label">User Name</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" id="username">
+                                </div>
+                            </div>
+                            <div class="row mb-2">
+                                <label for="password" class="col-sm-4 col-form-label">Password</label>
+                                <div class="col-sm-8">
+                                    <input type="password" class="form-control" id="password">
+                                </div>
+                            </div>
+                            <div class="row mb-2">
+
+                                <div class="col-sm-8">
+                                    <input type="submit" id="submit" class="btn btn-success btn-block">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </form>
+
+
+                <!--     HIGHLIGHTED POSTS  -->
+
+                <div class="list-group">
+                    <a href="" class="list-group-item">
+                        <h4 class="list-group-item-heading">Post1</h4>
+                        <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+                    </a>
+                </div>
+            </aside>
+
+        </div>
+</body>
+
+</html>
+
+
