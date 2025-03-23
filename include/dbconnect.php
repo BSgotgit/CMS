d9be11166a0b275a720399e06f949e48525e7781
BSgotgit <biswajitswain746@gmail.com>
2025-03-23 21:50:17 +0530

    Database error handling

--- include/dbconnect.php
+++ include/dbconnect.php
@@ -1,7 +1,12 @@
 <?php
 $hostname = 'localhost';
 $username = 'root';
-$password = '';
+$password = '1234';
 $database = 'CMS';
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
+
+if (mysqli_connect_errno()){
+    printf('Failed to connect '.mysqli_connect_error());
+}
+
