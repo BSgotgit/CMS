7008e67f59b0ba271b5e7bac7b79e1b2afb0d5c2
Debaa <debaa.dm@gmail.com>
2025-03-29 14:14:18 +0530

    db fixed

--- include/dbconnect.php
+++ include/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 $hostname = 'localhost';
 $username = 'root';
-$password = '';//12345
+$password = '12345';
 $database = 'CMS';
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
