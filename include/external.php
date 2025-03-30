58a403e232b8f3ef381b8170d0012deb6648327e
BSgotgit <biswajitswain746@gmail.com>
2025-03-30 12:13:48 +0530

    not required hence deleted

--- include/external.php
+++
@@ -1,7 +0,0 @@
-<!--for eternal js and css from bootstrap --->
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
