bca8c9ef2efeda09336fcfb1e6c625db415b752d
BSgotgit <biswajitswain746@gmail.com>
2025-04-04 15:47:02 +0530

    minor changes

--- include/dbconnect.php
+++ include/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 $hostname = 'localhost';
 $username = 'root';
-$password = '12345';
+$password = '';
 $database = 'CMS';
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
